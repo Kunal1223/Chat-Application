727498daa2e345616e5ff6b6aca36893fbd9967b
Kunal Kishor <kunalbth2002@gmail.com>
2023-09-01 15:44:30 +0530

    server

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
 
-app.listen(5000, (req , res) =>{
+const app = express();
+dotenv.config();
+
+app.get('/' , (req,res)=>{
+    res.send("This is about page by me");
+})
+
+app.get('/test' , (req,res)=>{
+    res.send("This is about page");
+})
+
+PORT = process.env.PORT
+app.listen(PORT, (req , res) =>{  
     console.log("Listing on the 5000 port");
 })
