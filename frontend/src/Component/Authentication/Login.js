1b1c5ef531614b87f0b41a892ac044d487eff8e4
Kunal Kishor <kunalbth2002@gmail.com>
2023-09-07 10:40:59 +0530

    login & signup

--- frontend/src/Component/Authentication/Login.js
+++ frontend/src/Component/Authentication/Login.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [Email, setEmail] = useState();
+  const [Password, setPassword] = useState();
+  const [Cpassword, setCpassword] = useState();
+  const [show, setShow] = useState(false);
+  const [cshow, setcShow] = useState(false);
+
+  const handleClick = () =>setShow(!show);
+  const handleClick1 = () =>setcShow(!cshow);
+
   return (
-    <div>
-      this is login
-    </div>
+    <VStack spacing="15px">
+    <FormControl id="email" isRequired>
+      <FormLabel>Email Address</FormLabel>
+      <Input
+        type="email"
+        placeholder="Enter Your Email Address"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+    </FormControl>
+    <FormControl id="password" isRequired>
+      <FormLabel>Password</FormLabel>
+      <InputGroup size="md">
+        <Input
+          type={show ? "text" : "password"}
+          placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? "Hide" : "Show"}  
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </FormControl>
+    <Button
+      colorScheme="red"
+      width="100%"
+      style={{ marginTop: 15 }}
+    >
+      Log in
+    </Button>
+  </VStack>
   )
 }
 
--- frontend/src/Component/Authentication/Signup.js
+++ frontend/src/Component/Authentication/Signup.js
@@ -1,31 +1,83 @@
-import { FormControl, FormLabel, Input, VStack } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 const Signup = () => {
-  const [Name , setName] = useState();
-  const [Email , setEmail] = useState();
-  const [Password , setPassword] = useState();
-  const [Cpassword , setCpassword] = useState();
-  const [Images , setImages] = useState();
+  const [Name, setName] = useState();
+  const [Email, setEmail] = useState();
+  const [Password, setPassword] = useState();
+  const [Cpassword, setCpassword] = useState();
+  const [Images, setImages] = useState();
+  const [show, setShow] = useState(false);
+  const [cshow, setcShow] = useState(false);
+
+
+
+  const handleClick = () =>setShow(!show);
+  const handleClick1 = () =>setcShow(!cshow);
 
 
   return (
-    <VStack spacing="1.5"> 
-      <FormControl id='first-Name' isRequired>
+    <VStack spacing="15px">
+      <FormControl id="first-name" isRequired>
         <FormLabel>Name</FormLabel>
-        <Input placeholder='Enter Your name' onChange={(e)=>setName(e.target.value)}/>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </FormControl>
-
-      <FormControl id='first-Name' isRequired>
-        <FormLabel>Email</FormLabel>
-        <Input placeholder='Enter Your Email' onChange={(e)=>setEmail(e.target.value)}/>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email Address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </FormControl>
-
-      <FormControl id='first-Name' isRequired>
+      <FormControl id="password" isRequired>
         <FormLabel>Password</FormLabel>
-        <Input placeholder='Enter Your Password' onChange={(e)=>setPassword(e.target.value)}/>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}  
+            </Button>
+          </InputRightElement>
+        </InputGroup>
       </FormControl>
-      
+      <FormControl id="password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={cshow ? "text" : "password"}
+            placeholder="Confirm password"
+            onChange={(e) => setCpassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick1}>
+              {cshow ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+      >
+        Sign Up
+      </Button>
     </VStack>
   )
 }
